86924fbe56ba60981d0c8f026222dcab5db30270
창현 <matthew624@kakao.com>
2021-01-14 13:59:00 +0000

    change folder

---
+++ ing/dynamic_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+	int N=0,cnt=0,i=0;
+	char *arr;
+	
+	scanf("%d",&N);
+	
+	arr=(char*)malloc(N*sizeof(char));
+	if(arr==NULL){
+		printf("ERROR");
+		return -1;
+	}
+	for(i=0;i<N;i++){
+		scanf("%c",arr[i]);
+	}
+	for(i=0;i<N;i++){
+		if((arr[i]=='c')&&(arr[i+1]=='a')&&(arr[i+2]=='t'))
+			cnt++;
+	}
+	printf("%d",cnt);
+	
+	
+	
+	return 0;
+}
