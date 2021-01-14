7932249316686501384099534a3d0cb2ba025456
창현 <matthew624@kakao.com>
2021-01-14 14:12:59 +0000

    동적할당1번

--- ing/dynamic_1.c
+++ ing/dynamic_1.c
@@ -7,16 +7,19 @@ int main(){
 	char *arr;
 	
 	scanf("%d",&N);
+	getchar();
 	
 	arr=(char*)malloc(N*sizeof(char));
 	if(arr==NULL){
 		printf("ERROR");
 		return -1;
 	}
+	
 	for(i=0;i<N;i++){
-		scanf("%c",arr[i]);
+		scanf("%c",&arr[i]);
 	}
-	for(i=0;i<N;i++){
+	
+	for(i=0;i<N-2;i++){
 		if((arr[i]=='c')&&(arr[i+1]=='a')&&(arr[i+2]=='t'))
 			cnt++;
 	}
