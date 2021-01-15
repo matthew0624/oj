9cf0e140d559e058cdcb34e84e481b4faff1bfbe
창현 <matthew624@kakao.com>
2021-01-15 13:35:53 +0000

    동적할당10번끝

---
+++ complete/dynamic_2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+	int N=0,len=0,min=100,i=0,j=0;
+	char *arr,*tmp;
+	
+	scanf("%d",&N);
+	getchar();
+	
+	arr=(char*)malloc(sizeof(char)*N);
+	if(arr==NULL){
+		printf("ERROR");
+		return -1;
+	}
+	
+	tmp=(char*)malloc(sizeof(char)*N);
+	if(tmp==NULL){
+		printf("ERROR");
+		return -1;
+	}
+	
+	for(i=0;i<N;i++){
+		fgets(arr,101,stdin);
+		while(1){
+			if(arr[j]!='\0'){
+				j++;
+			}
+			else{
+				printf("%d\n",j);
+				break;
+			}
+		}
+		if(j<min){
+			min=j;
+			strcpy(tmp,arr);
+		}	
+		j=0;
+	}
+	
+	printf("%s",tmp);
+	
+	free(arr);
+	free(tmp);
+	
+	return 0;
+}
